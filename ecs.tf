c4af8c70df43054fff5748556c968690e7651102
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-05-12 19:57:19 +0530

    Updated AWS account no.

--- ecs.tf
+++ ecs.tf
@@ -26,7 +26,7 @@ resource "aws_ecs_task_definition" "demo_ecs_app_def" {
   container_definitions = jsonencode([
     {
       name   = var.aws_ecr_repository
-      image  = "340442909088.dkr.ecr.ap-south-1.amazonaws.com/demo_ecs_app:latest"
+      image  = "851725279482.dkr.ecr.ap-south-1.amazonaws.com/demo_ecs_app:latest"
       cpu    = var.fargate_cpu
       memory = var.fargate_memory
       portMappings = [
